6a94798a0fe451bc929f7b2e959a317883985868
Matthew Lau <lau.cy.matthew@gmail.com>
2021-12-29 13:07:20 +0000

    feat: use Routes component

---
+++ src/config/Routes.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import PrivateRoute from "../config/PrivateRoute";
+import LandingPage from "../pages/LandingPage";
+import DashboardPage from "../pages/DashboardPage";
+import LoginPage from "../pages/LoginPage";
+import RegistrationPage from "../pages/RegistrationPage";
+import AddSkillPage from "../pages/AddSkillPage";
+import AddProjectPage from "../pages/AddProjectPage";
+import ProjectPage from "../pages/ProjectPage";
+import ProjectSkillsPage from "../pages/ProjectSkillsPage";
+import NotFoundPage from "../pages/NotFoundPage";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="sign-up" element={<RegistrationPage />} />
+      <Route
+        path="dashboard"
+        element={<PrivateRoute component={DashboardPage} />}
+      />
+      <Route
+        path="add-skill"
+        element={<PrivateRoute component={AddSkillPage} />}
+      />
+      <Route
+        path="add-project"
+        element={<PrivateRoute component={AddProjectPage} />}
+      />
+      <Route
+        path="projects/:id"
+        element={<PrivateRoute component={ProjectPage} />}
+      />
+      <Route
+        path="projects/:id/project-skills"
+        element={<PrivateRoute component={ProjectSkillsPage} />}
+      />
+      <Route path="/*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
