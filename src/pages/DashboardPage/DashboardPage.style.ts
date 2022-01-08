9dd9fff626f19e8a223ca8a212b95f83724d5574
Matthew Lau <lau.cy.matthew@gmail.com>
2022-01-08 10:27:06 +0000

    fix: add center styling to loader

--- src/pages/DashboardPage/DashboardPage.style.ts
+++ src/pages/DashboardPage/DashboardPage.style.ts
@@ -8,7 +8,10 @@ export default makeStyles(
         paddingBottom: spacing(3),
       },
       loader: {
-        margin: "auto auto",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
       },
       link: {
         color: palette.text.primary,
